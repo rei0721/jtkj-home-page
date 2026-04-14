ae02cb7725635849f78db221599fecb6089927f4
good-man <zhang@ixiaolin.com>
2026-04-14 21:12:37 +0800

    home_bg

--- src/data/home/heroSlides.ts
+++ src/data/home/heroSlides.ts
@@ -1,17 +1,18 @@
 export const heroSlides = [
   {
     id: 1,
-    image: 'https://images.unsplash.com/photo-1550751827-4bd374c3f58b?q=80&w=2070&auto=format&fit=crop',
+    image: '/images/home/bg_1.png',
     alt: 'Advanced Optoelectronic Technology',
+    
   },
   {
     id: 2,
-    image: 'https://images.unsplash.com/photo-1581091226825-a6a2a5aee158?q=80&w=2070&auto=format&fit=crop',
+    image: '/images/home/bg_2.png',
     alt: 'Smart Manufacturing Facility',
   },
   {
     id: 3,
-    image: 'https://images.unsplash.com/photo-1451187580459-43490279c0fa?q=80&w=2070&auto=format&fit=crop',
+    image: '/images/home/bg_3.png',
     alt: 'Global Connectivity',
   },
 ];
