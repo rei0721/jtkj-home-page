896ecb39adb130edbbbc658b09eee68437a2f628
kawaiirei0 <i@yushulinfeng.com>
2026-04-15 11:41:48 +0800

    ccccccc

--- src/constants/company.ts
+++ src/constants/company.ts
@@ -1,5 +1,5 @@
 export const COMPANY = {
-  name: '江天科技股份有限公司',
+  name: '江天科技有限公司',
   phone: '0818-8218666',
   email: 'jtkj@scjtkj.com',
   mobile: '+86 193-8289-5171',
@@ -10,5 +10,5 @@ export const COMPANY = {
   addresses: ['中国·深圳高新技术产业园区科技南路88号', '四川省达州市开江县普安镇经开大道'],
   icp: '粤ICP备88888888号',
   mapSrc:
-    'https://api.map.baidu.com/marker?location=31.085,107.868&title=光电产业园&content=四川省达州市开江县普安镇经开大道&output=html&src=webapp.baidu.openAPIdemo',
+    'https://map.baidu.com/poi/%E5%9B%9B%E5%B7%9D%E6%B1%9F%E5%A4%A9%E7%A7%91%E6%8A%80%E6%9C%89%E9%99%90%E5%85%AC%E5%8F%B8/@12003503.59,3623512.6,19z?uid=da3036c700a6398848cc4a31&ugc_type=3&ugc_ver=1&device_ratio=1&compat=1&sn_xy=11584526,3552106&en_uid=da3036c700a6398848cc4a31&pcevaname=pc4.1&querytype=detailConInfo&da_src=shareurl',
 } as const;
